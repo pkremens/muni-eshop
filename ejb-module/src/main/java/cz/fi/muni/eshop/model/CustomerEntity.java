ee73730f34369dcbfc1a1911cd3195f686fe456e
Petr Kremensky <pkremens@redhat.com>
2012-12-04 00:18:14 +0100

    picketlink dependency

--- ejb-module/src/main/java/cz/fi/muni/eshop/model/CustomerEntity.java
+++ ejb-module/src/main/java/cz/fi/muni/eshop/model/CustomerEntity.java
@@ -5,25 +5,140 @@
 package cz.fi.muni.eshop.model;
 
 import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
-/**
- *
- * @author Petr Kremensky <207855@mail.muni.cz>
- */
+import org.hibernate.validator.constraints.NotEmpty;
+import org.picketlink.idm.api.User;
 
 @Entity
-public class CustomerEntity implements Serializable {
+public class CustomerEntity implements User, Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     @Id
-    private Long id;
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message = "not a valid email address")
+    private String email;
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+    private String name;
+    @NotEmpty
+    @NotNull
+    private String password;
+    @NotEmpty
+    @NotNull
+    private String role;
 
-    public Long getId() {
-        return id;
+    public String getPasswordSubstring() {
+        return password.substring(0, 10);
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
     }
-  // TODO have to resolve dependecies for picketLink   
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CustomerEntity other = (CustomerEntity) obj;
+        if ((this.email == null) ? (other.email != null) : !this.email.equals(other.email)) {
+            return false;
+        }
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        if ((this.role == null) ? (other.role != null) : !this.role.equals(other.role)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + (this.email != null ? this.email.hashCode() : 0);
+        hash = 37 * hash + (this.name != null ? this.name.hashCode() : 0);
+        hash = 37 * hash + (this.role != null ? this.role.hashCode() : 0);
+        return hash;
+    }
+
+   
+
+    @Override
+    public String toString() {
+        return "email: " + email + " name: " + name;
+    }
+
+    public String toLog() {
+        return "Customer [email=" + email + ", name=" + name + ", password="
+                + getPasswordSubstring() + ", role=" + role + "]";
+    }
+
+    @Override
+    public String getKey() {
+        return getId();
+    }
+
+    @Override
+    public String getId() {
+        return getEmail();
+    }
 }
