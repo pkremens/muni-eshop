4d61253d8943251b743110ca4e91c5ab6f1f7938
Petr Kremensky <pkremens@redhat.com>
2012-12-07 14:40:06 +0100

    Authentization finished

--- web/src/main/java/cz/fi/muni/eshop/security/Authorization.java
+++ web/src/main/java/cz/fi/muni/eshop/security/Authorization.java
@@ -8,6 +8,8 @@ package cz.fi.muni.eshop.security;
 import cz.fi.muni.eshop.model.CustomerEntity;
 import cz.fi.muni.eshop.model.Role;
 import cz.fi.muni.eshop.security.permissions.Admin;
+import cz.fi.muni.eshop.security.permissions.BasicPermission;
+import cz.fi.muni.eshop.security.permissions.Seller;
 import cz.fi.muni.eshop.util.quilifier.MuniEshopLogger;
 import java.util.logging.Logger;
 import javax.inject.Inject;
@@ -24,10 +26,41 @@ public class Authorization {
     @MuniEshopLogger
     Logger log;
     
+    
+    /**
+     * 
+     * @param identity
+     * @return true, if user has ADMIN role, else false
+     */
     @Secures
     @Admin
     public boolean isAdmin(Identity identity) {
         return (identity.isLoggedIn() ? ((CustomerEntity)identity.getUser()).getRole().equals(Role.ADMIN) : false);
     }
+    
+    /**
+     * 
+     * @param identity
+     * @return true, if user has SELLER role, else false
+     */
+    @Secures
+    @Seller
+    public boolean isSeller(Identity identity) {
+        return (identity.isLoggedIn() ? ((CustomerEntity)identity.getUser()).getRole().equals(Role.SELLER) : false);
+    }
+    
+    
+    /**
+     * 
+     * @param identity
+     * @return true, if user has BASIC role, else false
+     */
+    @Secures
+    @BasicPermission
+    public boolean isBasic(Identity identity) {
+        return (identity.isLoggedIn() ? ((CustomerEntity)identity.getUser()).getRole().equals(Role.BASIC) : false);
+    }
+    
+    
 
 }
